ea736a68b0ea13bbb8458ae4827892cdd105e3a7
Keno <keno.mandrysch@gmail.com>
2026-04-16 12:40:21 +0200

    Challenge 4

---
+++ challenge4.js
@@ -0,0 +1,30 @@
+function missingLetter(input) {
+  //Sort input alphabetically
+  input.sort();
+
+  //Check if upper- or lowercase
+  const upperCase = input[0] === input[0].toUpperCase();
+
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  if (upperCase) {
+    alphabet = alphabet.toUpperCase();
+  }
+
+  let alphabetArray = alphabet.split("");
+
+  //Cut down alphabet array to start with the same letter as the input
+  alphabetArray.forEach((letter, index) => {
+    if (letter == input[0]) {
+      alphabetArray = alphabetArray.slice(index);
+    }
+  });
+
+  /*
+   * Only keep letters that are not included in the input array.
+   * Our alphabet starts with the same letter as the input and the input is alphabetically sorted.
+   * If we not remove all the letters from the input from the alphabet, the first letter remaining in the alphabet will be the missing one
+   */
+  alphabetArray = alphabetArray.filter((letter) => !input.includes(letter));
+
+  return alphabetArray[0];
+}
