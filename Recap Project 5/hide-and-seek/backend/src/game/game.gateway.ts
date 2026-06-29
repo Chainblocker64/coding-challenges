af7f6183b3b3030c775ade6beee3ce36facefb29
Keno <keno.mandrysch@gmail.com>
2026-06-29 17:35:23 +0200

    Role Management

--- Recap Project 5/hide-and-seek/backend/src/game/game.gateway.ts
+++ Recap Project 5/hide-and-seek/backend/src/game/game.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -6,8 +7,12 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { randomUUID, UUID } from 'node:crypto';
 import { Server, Socket } from 'socket.io';
 import { GameService } from './game.service';
+import type { Role } from './game.service';
+
+type Room = Record<string, Role>;
 
 @WebSocketGateway({
   cors: {
@@ -18,14 +23,69 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
+  rooms = new Map<UUID, Room>();
+
   constructor(private readonly gameService: GameService) {}
 
   handleConnection(client: Socket) {
     console.log(`Client connected: ${client.id}`);
+
+    const roomId = this.freeRoom();
+    client.join(roomId);
+
+    const role = this.newRole(roomId);
+
+    this.rooms.set(roomId, { ...room, [client.id]: role });
+
+    ////
+    void client.join(this.latestRoom.toString());
+    console.log(`Client ${client.id} joined room ${this.latestRoom}`);
+
+    console.log(
+      `User count for room ${this.latestRoom}} is currently ${this.currentUserCount()}`,
+    );
   }
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
+  }
+
+  userCount(roomId: string): number {
+    return this.server.sockets.adapter.rooms.get(roomId)?.size || 0;
+  }
+
+  freeRoom() {
+    this.rooms.forEach((room, roomId) => {
+      if (this.userCount(roomId) < 2) {
+        return roomId;
+      }
+    });
+
+    return randomUUID();
+  }
+
+  newRole(roomId: UUID): Role {
+    const room = this.rooms.get(roomId);
+
+    let role = '';
+
+    if (room) {
+      for (const [clientRole] of Object.values(room)) {
+        if (clientRole === 'seeker') {
+          role = 'hider';
+        } else {
+          role = 'seeker';
+        }
+      }
+    } else {
+      if (Math.random() >= 0.5) {
+        role = 'seeker';
+      } else {
+        role = 'seeker';
+      }
+    }
+
+    return role;
   }
 
   @SubscribeMessage('login')
